ddc7cede05f57073b25e0403773018fb2c7a6c63
HamzaMoslah <hamzamaslah@gmail.com>
2017-12-14 10:40:24 +0100

    Initial commit

---
+++ src/test/java/tn/insat/fix/fixserver/FixserverApplicationTests.java
@@ -0,0 +1,16 @@
+package tn.insat.fix.fixserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FixserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
